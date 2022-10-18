bcf02ee60fd4b2fb27977c744f7e464dbaf754ef
Leo <leo@youuy.com>
2022-10-18 14:52:38 +0800

    init

---
+++ gradle-docker-plugin/src/main/groovy/com/youuy/gradle/plugin/docker/DockerPlugin.groovy
@@ -0,0 +1,213 @@
+package com.youuy.gradle.plugin.docker;
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.jar.Attributes
+import java.util.jar.Manifest
+
+class DockerPlugin implements Plugin<Project> {
+    private final static String EXTENSION_DOCKER = "docker"
+    private final static String TASK_BUILD_IMAGE = "dockerBuild"
+    private final static String TASK_PUSH_IMAGE = "dockerPush"
+    private final static String DEFAULT_CONTEXT = "docker"
+    @Override
+    void apply(Project project) {
+        def extension = project.extensions.create(EXTENSION_DOCKER, DockerPluginExtension.class, project)
+        project.task(TASK_BUILD_IMAGE) {
+            group = EXTENSION_DOCKER
+            dependsOn project.tasks.getByName("bootJar")
+            doLast {
+                String context
+                //user setting's folder
+                String userSettingContext = extension.to.context.getOrNull()
+                boolean needGen = false
+                if (userSettingContext){
+                    if (Files.exists(Path.of(userSettingContext))){
+                        if (!Files.isDirectory(Path.of(userSettingContext))){
+                            throw new RuntimeException("The Parameter 'docker.to.context' is not a directory.")
+                        }else{
+                            logger.info("用户定义工作目录为：$userSettingContext")
+                            context = userSettingContext
+                            needGen = false
+                        }
+                    } else{
+                        logger.info("用户定义工作目录为：$userSettingContext ，但该目录不存在，自动生成文件模板")
+                        context = userSettingContext
+                        needGen = true
+                    }
+                }else{
+                    //user settings not defined
+
+                    //sub project setting
+                    String subProjectContext = Path.of(project.projectDir.getPath(), DEFAULT_CONTEXT)
+                    //root project setting
+                    String rootProjectContext = Path.of(project.rootDir.getPath(), DEFAULT_CONTEXT)
+                    if (Files.exists(Path.of(subProjectContext))) {
+                        if (Files.isDirectory(Path.of(subProjectContext))){
+                            //use subProjectContext
+                            context = subProjectContext
+                            needGen = false
+                        }else{
+                            throw new RuntimeException("The Parameter '$subProjectContext' is not a directory.")
+                        }
+                    }else if (Files.exists(Path.of(rootProjectContext))){
+                        if (Files.isDirectory(Path.of(rootProjectContext))){
+                            //use rootProjectContext
+                            context = rootProjectContext
+                            needGen = false
+                        }else{
+                            throw new RuntimeException("The Parameter '$rootProjectContext' is not a directory.")
+                        }
+                    }else{
+                        //use subProjectContext and gen
+                        context = subProjectContext
+                        needGen = true
+                    }
+                }
+                if (needGen){
+                    logger.warn("generate a new docker folder in $context")
+                    File contextFolder = new File(context)
+                    contextFolder.mkdirs()
+                    File dockerfile = new File(contextFolder, "Dockerfile")
+                    dockerfile.write(generateDockerfile())
+                    File entrypoint = new File(contextFolder, "entrypoint.sh")
+                    entrypoint.write(generateEntrypoint())
+                }
+                //build
+                //1. delete last
+                File tmp = new File(context, ".tmp")
+                project.delete(tmp)
+                tmp.mkdirs()
+                File unjar = new File(project.buildDir, "unjar")
+                project.delete(unjar)
+                unjar.mkdirs()
+                //2.unjar jar
+                project.exec {
+                    workingDir unjar
+                    executable "jar"
+                    args "-xf", "../libs/${project.name}${project.version ? "-${project.version}" : ""}.jar"
+                }
+                //3.copy unjar to context
+                project.copy {
+                    from unjar
+                    include "BOOT-INF/**", "META-INF/**"
+                    into tmp.getPath()
+                }
+                //4. get Start-Class
+                Manifest manifest = new Manifest(Files.newInputStream(new File(unjar, "META-INF/MANIFEST.MF").toPath()))
+                Attributes attr = manifest.getMainAttributes()
+                String startClass = attr.getValue("Start-Class")
+                File startClassFile = new File(tmp, "START_CLASS")
+                startClassFile.write(startClass)
+
+                if (!extension.to.image){
+                    throw new RuntimeException("The Parameter 'to.image' cannot be null")
+                }
+
+                //5. build
+                project.exec {
+                    workingDir context
+                    def tags = extension.to.tags ? extension.to.tags : null
+                    def commandArgs = ["build"]
+                    if (tags && tags.size() > 0) {
+                        tags.get().each {tag ->
+                            commandArgs << "-t"
+                            commandArgs << "${extension.to.image.get()}:${tag}"
+                        }
+                    }
+                    commandArgs << "-t"
+                    commandArgs << "${extension.to.image.get()}:${project.version}"
+
+                    commandArgs << "-t"
+                    commandArgs << "${extension.to.image.get()}:latest"
+
+                    def baseImage = extension.from.image ? extension.from.image : null
+                    if (baseImage){
+                        commandArgs << "--build-arg"
+                        commandArgs << "BASE_IMAGE=${baseImage.get()}"
+                    }
+                    def ports = extension.container.ports ? extension.container.ports : null
+                    if (ports && ports.size() > 0) {
+                        String portsStr = ""
+                        ports.get().each {item ->
+                            if (portsStr.length() > 0){
+                                portsStr += ","
+                            }
+                            portsStr += String.valueOf(item)
+                        }
+                        commandArgs << "--build-arg"
+                        commandArgs << "PORT=${portsStr}"
+                    }
+                    commandArgs << "--no-cache"
+                    commandArgs << "."
+
+                    executable "docker"
+                    println(commandArgs)
+                    args commandArgs
+                }
+            }
+        }
+
+        project.task(TASK_PUSH_IMAGE) {
+            group = EXTENSION_DOCKER
+            doLast {
+                project.exec {
+                    executable "docker"
+                    args "push", "-a", extension.to.image.get()
+                }
+            }
+        }
+    }
+
+    String generateDockerfile(){
+        return """
+ARG BASE_IMAGE=youuy/openjre:11
+
+FROM youuy/openjdk:11 as builder
+WORKDIR /workspace/
+ARG MODULE
+COPY tmp/*.jar .
+COPY Tool.class .
+RUN mkdir -p dependency && (cd dependency; jar -xf ../*.jar) \\
+    && java Tool dependency/META-INF/MANIFEST.MF > dependency/MAIN_CLASS
+
+FROM \$BASE_IMAGE
+VOLUME /tmp
+ARG PORT=8080
+EXPOSE \$PORT
+WORKDIR /app
+COPY entrypoint.sh /app/entrypoint.sh
+COPY --from=builder /workspace/dependency/MAIN_CLASS /app/MAIN_CLASS
+COPY --from=builder /workspace/dependency/BOOT-INF/lib /app/lib
+COPY --from=builder /workspace/dependency/META-INF /app/META-INF
+COPY --from=builder /workspace/dependency/BOOT-INF/classes /app
+ENTRYPOINT ["/app/entrypoint.sh"]
+HEALTHCHECK --start-period=10s --interval=10s --timeout=3s --retries=5 \\
+            CMD curl --silent --fail --request GET http://localhost:\$PORT/actuator/health \\
+                | jq --exit-status '.status == "UP"' || exit 1
+"""
+    }
+    String generateEntrypoint(){
+        return """
+#!/bin/sh
+# check SPRING_PROFILES_ACTIVE environment variable
+[ -z "\$SPRING_PROFILES_ACTIVE" ] && echo "Error: Define SPRING_PROFILES_ACTIVE environment variable" && exit 1;
+# setup main class
+export MAIN_CLASS=`cat /app/MAIN_CLASS`
+# startup
+java -XX:+UseContainerSupport \\
+-XX:InitialRAMPercentage=50 \\
+-XX:MaxRAMPercentage=85 \\
+-XX:+UnlockExperimentalVMOptions \\
+-Djava.security.egd=file:/dev/./urandom \\
+-Dlog4j2.formatMsgNoLookups=true \\
+--add-opens=java.base/jdk.internal.loader=ALL-UNNAMED \\
+--add-opens=java.base/java.net=ALL-UNNAMED \\
+-cp /app:/app/lib/* \$MAIN_CLASS
+"""
+    }
+
+}
---
+++ gradle-docker-plugin/src/main/groovy/com/youuy/gradle/plugin/docker/DockerPluginExtension.groovy
@@ -0,0 +1,91 @@
+package com.youuy.gradle.plugin.docker
+
+import groovy.transform.CompileStatic
+import org.gradle.api.Action
+import org.gradle.api.Project
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.Property
+import org.gradle.api.provider.SetProperty
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Optional
+
+import javax.inject.Inject
+
+@CompileStatic
+class DockerPluginExtension {
+    FromParameter from
+    ToParameter to
+    ContainerParameter container
+
+    @Inject
+    DockerPluginExtension(Project project) {
+        ObjectFactory factory = project.getObjects()
+        from = factory.newInstance(FromParameter.class)
+        to = factory.newInstance(ToParameter.class)
+        container = factory.newInstance(ContainerParameter.class)
+    }
+
+    public void from(Action<? super FromParameter> action) {
+        action.execute(from);
+    }
+
+    public void to(Action<? super ToParameter> action) {
+        action.execute(to);
+    }
+
+    public void container(Action<? super ContainerParameter> action) {
+        action.execute(container);
+    }
+}
+
+
+@CompileStatic
+class FromParameter {
+    @Input
+    @Optional
+    Property<String> image
+
+    @Inject
+    FromParameter(ObjectFactory objectFactory) {
+        image = objectFactory.property(String.class)
+        image.set("openjdk:11-jre")
+    }
+}
+
+@CompileStatic
+class ToParameter {
+    @Input
+    Property<String> image
+    @Input
+    @Optional
+    SetProperty<String> tags
+    @Input
+    @Optional
+    Property<String> context
+
+    @Inject
+    ToParameter(ObjectFactory objectFactory) {
+        image = objectFactory.property(String.class)
+        tags = objectFactory.setProperty(String).empty()
+        tags.set(["latest"])
+        context = objectFactory.property(String.class)
+    }
+}
+
+@CompileStatic
+class ContainerParameter {
+    @Input
+    @Optional
+    SetProperty<Integer> ports
+    @Input
+    @Optional
+    Property<String> entrypoint
+
+    @Inject
+    ContainerParameter(ObjectFactory objectFactory) {
+        ports = objectFactory.setProperty(Integer).empty()
+        ports.set([8080])
+        entrypoint = objectFactory.property(String.class)
+        entrypoint.set("/entrypoint.sh")
+    }
+}
